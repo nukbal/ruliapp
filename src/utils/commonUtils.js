06a4bb2682c25b875966d0556609ff392c093bd6
Nukbal <wolfclaw@Nukbals-MacBook-Pro.local>
2018-03-05 23:31:17 +0900

    add throttle function

--- src/utils/commonUtils.js
+++ src/utils/commonUtils.js
@@ -28,6 +28,27 @@ export function debounce(func, wait, immediate) {
 	};
 };
 
+export const throttle = (func, limit) => {
+  let lastFunc
+  let lastRan
+  return function() {
+    const context = this
+    const args = arguments
+    if (!lastRan) {
+      func.apply(context, args)
+      lastRan = Date.now()
+    } else {
+      clearTimeout(lastFunc)
+      lastFunc = setTimeout(function() {
+        if ((Date.now() - lastRan) >= limit) {
+          func.apply(context, args)
+          lastRan = Date.now()
+        }
+      }, limit - (Date.now() - lastRan))
+    }
+  }
+};
+
 export function mergeArray(arr1, arr2) {
   return [...new Set([].concat(arr1, arr2))];
 }
