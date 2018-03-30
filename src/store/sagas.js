8d85d34d956c676fb07e79374d588c5f24b6357c
Nukbal <wolfclaw@Nukbals-MacBook-Pro.local>
2018-03-31 01:01:47 +0900

    remove image cache control via js

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,12 +1,10 @@
 import { all } from 'redux-saga/effects';
 import { boardSagas } from './ducks/boards';
 import { detailSaga } from './ducks/detail';
-import { cacheSaga } from './ducks/cache';
 
 export default function* rootSaga() {
   yield all([
     ...boardSagas,
     ...detailSaga,
-    ...cacheSaga,
   ]);
 }
