b6d7ca9f56458500be956cac74ccce1985b8b2ae
Nukbal <wolfclaw@Nukbals-MacBook-Pro.local>
2018-04-04 01:48:35 +0900

    resolve parser not working actual device

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -54,7 +54,7 @@ const formatContentNode = (item, key) => {
     } else if (item.tagName === 'iframe') {
       type = 'embed';
       content = item.attributes.src;
-    } else if (item.tagName === 'a' && item.childNodes[0].tagName === 'img') {
+    } else if (item.tagName === 'a' && item.childNodes.length && item.childNodes[0].tagName === 'img') {
       type = 'image';
       content = item.childNodes[0].attributes.src;
     } else if (item.tagName === 'a') {
@@ -68,17 +68,14 @@ const formatContentNode = (item, key) => {
           type = 'text';
           content = text;
         }
-      } else if (child.childNodes.length && child.childNodes[0].type === 'text') {
-        const text = child.childNodes[0].data && child.childNodes[0].data.trim();
-        if (text) {
-          type = 'text';
-          content = text;
-        }
+      } else if (child.rawText) {
+        type = 'text';
+        content = child.rawText.replace('&nbsp;', '');
       }
     }
   } else if (item.rawText) {
     type = 'text';
-    content = item.rawText;
+    content = item.rawText.replace('&nbsp;', '');
   }
 
   if (type && content) {
@@ -101,7 +98,6 @@ export const parseDetail = (htmlString) => {
   const contentStartIndex = htmlString.indexOf('<div class="view_content"');
   const contentEndIndex = htmlString.indexOf('<div class="notice_read_bottom');
   let html = htmlString.substring(contentStartIndex, contentEndIndex);
-  html = html.replace('&nbsp;', '').replace('<p></p>', '');
   const $ = loadHtml(html);
 
   let _node = $.querySelector('.view_content').removeWhitespace();
@@ -162,63 +158,16 @@ const formatBoardRow = (nodes) => {
   const link = subject.attributes.href.replace('http://bbs.ruliweb.com/', '');
 
   result.id = link.substring(link.lastIndexOf('/') + 1, link.length);
-  result.subject = subject.text;
+  result.title = subject.rawText.trim();
   result.prefix = link.substring(0, link.indexOf('/'));
   result.boardId = link.substring(link.indexOf('board/') + 6, link.indexOf('/read'));
 
-  result.comments = nodes.querySelector('span.num').text;
+  const comments = nodes.querySelector('span.num');
+  if (comments) result.comments = comments.text;
   result.author = nodes.querySelector('td.writer').text;
   result.likes = nodes.querySelector('td.recomd').text;
   result.views = nodes.querySelector('td.hit').text;
   result.times = nodes.querySelector('td.time').text;
-
-  // for (let i = 0; i < length; i++) {
-  //   const item = nodes.childNodes[i];
-  //   if (item.type !== 'tag') continue;
-
-  //   if (['board_name', 'divsn'].indexOf(item.attribs.class) !== -1) {
-  //     result.type = item.childNodes[0].data;
-  //   } else if (item.attribs.class.indexOf('subject') !== -1) {
-  //     let subject = item.childNodes[1];
-  //     let comment = item.childNodes[3];
-  //     if (subject.attribs.class === 'relative') {
-  //       subject = item.childNodes[1].childNodes[1];
-  //       let flag = true;
-  //       for (let j = 0, len = item.childNodes[1].childNodes.length; j < len; j++) {
-  //         if (item.childNodes[1].childNodes[j].name === 'span') {
-  //           comment = item.childNodes[1].childNodes[j];
-  //           flag = false;
-  //           break;
-  //         }
-  //       }
-  //       if (flag) comment = null;
-  //     }
-  //     const link = subject.attribs.href.replace('http://bbs.ruliweb.com/', '');
-  //     const id = link.substring(link.lastIndexOf('/') + 1, link.length);
-  //     const prefix = link.substring(0, link.indexOf('/'));
-  //     const boardId = link.substring(link.indexOf('board/') + 6, link.indexOf('/read'));
-  
-  //     result.title = subject.childNodes[0].data;
-  //     if (comment) {
-  //       result.comments = comment.childNodes[1].childNodes[0].data;
-  //     } else {
-  //       result.comments = 0;
-  //     }
-  //     result.prefix = prefix;
-  //     result.boardId = boardId;
-  //     result.id = id;
-  //   } else if (item.attribs.class.indexOf('writer') !== -1) {
-  //     result.author = item.childNodes[0].data.trim();
-  //   } else if (item.attribs.class.indexOf('recomd') !== -1) {
-  //     result.likes = item.childNodes[0].data.trim();
-  //   } else if (item.attribs.class.indexOf('hit') !== -1) {
-  //     result.views = item.childNodes[0].data.trim();
-  //   } else if (item.attribs.class.indexOf('time') !== -1) {
-  //     result.times = item.childNodes[0].data.trim();
-  //   } else if (item.attribs.class.indexOf('id') !== -1) {
-  //     result.id = item.childNodes[0].data.trim();
-  //   }
-  // }
 
   result.key = `${result.prefix}_${result.boardId}_${result.id}`;
   return result;
