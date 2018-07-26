91cb56347672033d615bfe1842ed4a9a38959126
WOLFCLAW <frostwolfclaw@gmail.com>
2018-07-26 16:26:21 +0900

    add html-parser

---
+++ app/utils/__test__/htmlParser.test.ts
@@ -0,0 +1,10 @@
+import fs from 'fs';
+import { resolve } from 'path';
+import parser from '../htmlParser';
+
+describe('html parser', () => {
+  it('works', () => {
+    const html = fs.readFileSync(resolve(__dirname, './html/hit_sample.txt'), 'utf-8');
+    parser(html);
+  });
+});
