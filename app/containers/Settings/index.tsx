b50fb484fc77be1351f27cdf5726c4204930a131
WOLFCLAW <frostwolfclaw@gmail.com>
2019-01-31 15:00:50 +0900

    image size will updated on focus

--- app/containers/Settings/index.tsx
+++ app/containers/Settings/index.tsx
@@ -1,15 +1,38 @@
-import React, { PureComponent } from 'react';
-import { View, Text, FlatList, ListRenderItem } from 'react-native';
-import { SafeAreaView } from 'react-navigation';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { SafeAreaView, NavigationScreenProp, NavigationEventCallback } from 'react-navigation';
 import styles from './styles';
 import ImageCache from './ImageCache';
 
-export default class Setting extends PureComponent {
-  renderItem = ({  }: ListRenderItem<number>) => {
+interface Props {
+  navigation: NavigationScreenProp<any, { title: string, key: string }>;
+}
 
+export default class Setting extends Component<Props> {
+  state = { show: true };
+  listener: any;
+  listener2: any;
+
+  componentDidMount() {
+    this.listener = this.props.navigation.addListener('didFocus', this._handleEvent);
+    this.listener2 = this.props.navigation.addListener('willBlur', this._handleEvent);
+  }
+
+  componentWillUnmount() {
+    this.listener.remove();
+    this.listener2.remove();
+  }
+
+  shouldComponentUpdate(_: Props, state: { show: boolean }) {
+    return this.state.show !== state.show;
+  }
+
+  _handleEvent: NavigationEventCallback = (evt) => {
+    this.setState({ show: evt.type === 'didFocus' });
   }
 
   render() {
+    if (!this.state.show) return null;
     return (
       <SafeAreaView style={styles.wrapper}>
         <View style={styles.header}>
