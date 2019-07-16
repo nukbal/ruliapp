e2e23e5b8282e9ac79b5a39ae50eaa6c4e6e5aaa
nukbal <frostwolfclaw@gmail.com>
2019-07-16 18:14:01 +0900

    automatically remove post cache on launch

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createAppContainer } from 'react-navigation';
 import { ThemeProvider } from 'styled-components/native';
+import AsyncStorage from '@react-native-community/async-storage';
 import lightTheme from './styles/light';
 import darkTheme from './styles/dark';
 import MainRouter from './pages';
@@ -9,6 +10,17 @@ const Container = createAppContainer(MainRouter);
 
 export default function App() {
   const theme = darkTheme;
+
+  useEffect(() => {
+    AsyncStorage.getAllKeys((err, keys) => {
+      if (err) return;
+      if (keys) {
+        AsyncStorage
+          .multiRemove(keys.filter(key => key.indexOf('@Post') > -1))
+      }
+    });
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <Container screenProps={{ theme }} />
