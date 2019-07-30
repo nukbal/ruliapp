193aeb081e34c69c6624a090935724ca8902765b
nukbal <frostwolfclaw@gmail.com>
2019-07-30 12:09:41 +0900

    feat: change post placeholders to looks okay

--- app/pages/Post/Comments/placeholder.tsx
+++ app/pages/Post/Comments/placeholder.tsx
@@ -5,11 +5,11 @@ import styles from './styles';
 
 export default function CommentPlaceholder() {
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { flex: 0, height: 135 }]}>
       <View style={styles.UserContainer}>
         <Placeholder width="30%" />
       </View>
-      <View style={styles.CommentContainer}>
+      <View style={[styles.UserContainer, { flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start' }]}>
         <Placeholder width="85%" />
         <Placeholder width="65%" />
       </View>
--- app/pages/Post/placeholder.tsx
+++ app/pages/Post/placeholder.tsx
@@ -3,17 +3,24 @@ import { View } from 'react-native';
 
 import styles from './styles';
 import Placeholder from '../Placeholder';
+import Comments from './Comments/placeholder';
 import ThemeContext from '../../ThemeContext';
 
 export default function PostPlaceholder() {
   const { theme } = useContext(ThemeContext);
   return (
     <View style={[styles.container, { backgroundColor: theme.background }]}>
-      <View style={[{ paddingTop: 16, paddingBottom: 16 }]}>
+      <View style={[{ padding: 8, paddingTop: 16 }]}>
         <Placeholder size={200} />
         <Placeholder />
         <Placeholder />
         <Placeholder width="75%" />
+      </View>
+      <View style={[styles.infoPanel, { flex: 0, backgroundColor: theme.primary, opacity: 0.65, height: 50 }]} />
+      <View>
+        <Comments />
+        <Comments />
+        <Comments />
       </View>
     </View>
   );
