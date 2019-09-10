889281f9c088fd41d0a83427975835aa533d5790
nukbal <frostwolfclaw@gmail.com>
2019-09-10 14:28:05 +0900

    update dependencies

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import BoardStack from './Board';
 import BoardList from './BoardList';
 import PostScreen from './Post';
